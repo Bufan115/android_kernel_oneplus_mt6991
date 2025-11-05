e00f831c39510513f185a30551c7598c2827dc5d
Bufan115 <115609554+Bufan115@users.noreply.github.com>
2025-11-05 13:56:53 +0800

    Update process.c

--- drivers/khack/process.c
+++ drivers/khack/process.c
@@ -4,6 +4,7 @@
 #include <linux/tty.h>
 #include <linux/mm.h>
 #include <linux/version.h>
+#include <linux/fs.h>
 
 #define ARC_PATH_MAX 256
 
@@ -15,53 +16,52 @@ extern void mmput(struct mm_struct *);
 
 uintptr_t get_module_base(pid_t pid, char *name)
 {
-	struct pid *pid_struct;
-	struct task_struct *task;
-	struct mm_struct *mm;
-	struct vm_area_struct *vma;
-	uintptr_t base_addr = 0;
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0))
-	struct vma_iterator vmi;
-#endif
+    struct pid *pid_struct;
+    struct task_struct *task;
+    struct mm_struct *mm;
+    struct vm_area_struct *vma;
+    uintptr_t base_addr = 0;
 
-	pid_struct = find_get_pid(pid);
-	if (!pid_struct)
-	{
-		return 0;
-	}
-	task = get_pid_task(pid_struct, PIDTYPE_PID);
-	if (!task)
-	{
-		return 0;
-	}
-	mm = get_task_mm(task);
-	if (!mm)
-	{
-		return 0;
-	}
+    pid_struct = find_get_pid(pid);
+    if (!pid_struct)
+    {
+        return 0;
+    }
+    
+    task = get_pid_task(pid_struct, PIDTYPE_PID);
+    if (!task)
+    {
+        return 0;
+    }
+    
+    mm = get_task_mm(task);
+    if (!mm)
+    {
+        return 0;
+    }
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0))
-	vma_iter_init(&vmi, mm, 0);
-	for_each_vma(vmi, vma)
-#else
-	for (vma = mm->mmap; vma; vma = vma->vm_next)
-#endif
-	{
-		char buf[ARC_PATH_MAX];
-		char *path_nm = "";
+    // 使用兼容的 VMA 遍历方式
+    for (vma = mm->mmap; vma; vma = vma->vm_next)
+    {
+        char *path_nm = NULL;
 
-		if (vma->vm_file)
-		{
-			path_nm =
-				file_path(vma->vm_file, buf, ARC_PATH_MAX - 1);
-			if (!strcmp(kbasename(path_nm), name))
-			{
-				base_addr = vma->vm_start;
-				break;
-			}
-		}
-	}
+        if (vma->vm_file)
+        {
+            char *dentry_path = d_path(&vma->vm_file->f_path, (char *)__get_free_page(GFP_KERNEL), PAGE_SIZE);
+            if (!IS_ERR(dentry_path))
+            {
+                path_nm = kbasename(dentry_path);
+                if (path_nm && !strcmp(path_nm, name))
+                {
+                    base_addr = vma->vm_start;
+                    free_page((unsigned long)dentry_path);
+                    break;
+                }
+                free_page((unsigned long)dentry_path);
+            }
+        }
+    }
 
-	mmput(mm);
-	return base_addr;
+    mmput(mm);
+    return base_addr;
 }
